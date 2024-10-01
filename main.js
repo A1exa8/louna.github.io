1382bc00199f7f0790a6de273a7711048f6f4b3f
A1exa8 <bitkova228@gmail.com>
2024-10-01 14:23:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+ const button = document.querySelector('.btn');
+ const catImage = document.getElementById('catImage');
+
+ button.addEventListener('mouseenter', () => {
+     catImage.style.display = 'block';
+ });
+
+ button.addEventListener('mouseleave', () => {
+     catImage.style.display = 'none';
+ });
+  const showVideoBtn = document.getElementById('showVideoBtn');
+  const videoContainer = document.getElementById('videoContainer');
+  const myVideo = document.getElementById('myVideo');
+
+  showVideoBtn.addEventListener('click', () => {
+      if (videoContainer.style.display === 'none' || videoContainer.style.display === '') {
+          videoContainer.style.display = 'block';
+          showVideoBtn.textContent = 'Meow?';
+          myVideo.play(); 
+      } else {
+          videoContainer.style.display = 'none';
+          showVideoBtn.textContent = 'Meow.';
+          myVideo.pause();
+      }
+  });
